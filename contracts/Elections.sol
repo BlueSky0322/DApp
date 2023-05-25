c7bec40f505e1d17fb4996e646850d2d4b605905
BlueSky0322 <ryanng8628@gmail.com>
2023-05-25 16:25:37 +0800

    chore: added candidates

--- contracts/Elections.sol
+++ contracts/Elections.sol
@@ -1,13 +1,29 @@
-// SPDX-License-Identifier: MIT 
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Elections {
+    // Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
 
-    // Read candidate
-    string public candidate;
+    // Store Candidates
+    // Fetch Candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store Candidates Count
+    uint public candidatesCount;
 
     // Constructor
-    constructor() public { 
-        candidate = "Candidate 1";
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
